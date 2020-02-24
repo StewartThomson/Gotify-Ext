d215a760f155879299ccac024b3dec197b8eecdd
Stewart Thomson <stewartthomson3@gmail.com>
2020-02-23 22:29:24 -0500

    nvm. needed it.

--- src/app/classes/gotify-socket.ts
+++ src/app/classes/gotify-socket.ts
@@ -12,8 +12,11 @@ export class GotifySocket {
   }
 
   public OpenConnection(token: string) {
+    let url = this.url.replace("http://", "ws://");
+    url = url.replace("https://", "wss://");
+
     this.token = token;
-    const newWebSocket = webSocket(this.url + "/stream?token=" + token);
+    const newWebSocket = webSocket(url + "/stream?token=" + token);
     this.connection = newWebSocket;
     this.messages = newWebSocket.asObservable().pipe<Message>(
       map<any, Message>((data: string) => {
