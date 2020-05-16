df7aafb50ceef184b9933282230a0163f1fcc5d3
Stewart Thomson <stewartthomson3@gmail.com>
2020-05-16 19:18:39 -0400

    background script no longer runs exclusively on install

--- src/background.ts
+++ src/background.ts
@@ -3,45 +3,43 @@ import {takeUntil} from "rxjs/operators";
 import {GotifySocket} from "./app/classes/gotify-socket";
 import {SocketService} from "./app/services/socket.service";
 
-chrome.runtime.onInstalled.addListener(() => {
-  const socket = new SocketService();
+const socket = new SocketService();
 
-  const destroy$: Subject<boolean> = new Subject<boolean>();
-  let connections: GotifySocket[];
-  socket.loadConnections().then(() => {
-    // I don't know why, but if I remove this, the counter in the notifications breaks. Maybe I'll look into it some day
-    socket.sockets.subscribe((sockets) => {
-      connections = sockets;
-    });
+const destroy$: Subject<boolean> = new Subject<boolean>();
+let connections: GotifySocket[];
+socket.loadConnections().then(() => {
+  // I don't know why, but if I remove this, the counter in the notifications breaks. Maybe I'll look into it some day
+  socket.sockets.subscribe((sockets) => {
+    connections = sockets;
+  });
 
-    let unreadCount = 0;
-    let isOpen = false;
+  let unreadCount = 0;
+  let isOpen = false;
 
-    chrome.runtime.onConnect.addListener((port) => {
-      isOpen = true;
-      chrome.browserAction.setBadgeText({text: ""});
-      port.onDisconnect.addListener(() => {
-        unreadCount = 0;
-        isOpen = false;
-      });
-    });
-
-    socket.$.pipe(takeUntil(destroy$)).subscribe((gotify: GotifySocket) => {
-      gotify.GetMessageSubscription().subscribe((_) => {
-        if (!isOpen) {
-          unreadCount++;
-          chrome.browserAction.setBadgeText({text: unreadCount.toString()});
-        }
-      });
+  chrome.runtime.onConnect.addListener((port) => {
+    isOpen = true;
+    chrome.browserAction.setBadgeText({text: ""});
+    port.onDisconnect.addListener(() => {
+      unreadCount = 0;
+      isOpen = false;
     });
   });
 
-  chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (changes.connections) {
-      const newConnections = changes.connections.newValue || [];
-      for (const connectionInfo of newConnections) {
-        socket.open(connectionInfo.url, connectionInfo.token);
+  socket.$.pipe(takeUntil(destroy$)).subscribe((gotify: GotifySocket) => {
+    gotify.GetMessageSubscription().subscribe((_) => {
+      if (!isOpen) {
+        unreadCount++;
+        chrome.browserAction.setBadgeText({text: unreadCount.toString()});
       }
-    }
+    });
   });
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (changes.connections) {
+    const newConnections = changes.connections.newValue || [];
+    for (const connectionInfo of newConnections) {
+      socket.open(connectionInfo.url, connectionInfo.token);
+    }
+  }
 });
